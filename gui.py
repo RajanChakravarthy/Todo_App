1d120cd5520c529f85a89b8414f9c84ab19d1836
rajan Mahadevan <rajanchakravarthy@gmail.com>
2023-09-20 10:45:52 +0200

    Add function with implementation

--- gui.py
+++ gui.py
@@ -2,11 +2,21 @@ import functions
 import PySimpleGUI as sg
 
 label_value = sg.Text('Type in a To-Do')
-input_button = sg.InputText(tooltip='Enter todo.')
+input_button = sg.InputText(tooltip='Enter todo.', key='todo')
 add_button = sg.Button('Add')
 
 window = sg.Window('To-Do App',layout=[[label_value],
                                             [input_button, add_button]])
 
-window.read()
+while True:
+    event, value = window.read()
+    match event:
+        case 'Add':
+            todo = value['todo']
+            todos = functions.get_todo()
+            todos.append(todo)
+            functions.write_todos(todos)
+        case sg.WIN_CLOSED:
+            break
+
 window.close()
